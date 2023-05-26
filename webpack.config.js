05c5072baaa9b1e8db73caddf5e780f325090c33
David Gootman <gootman@gmail.com>
2023-05-25 20:22:52 -0700

    Extract CSS to chunk

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'production',
@@ -8,6 +9,9 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             title: 'ASearch',
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash:8].css',
         }),
     ],
     module: {
@@ -19,7 +23,7 @@ module.exports = {
             },
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
             },
         ],
     },
